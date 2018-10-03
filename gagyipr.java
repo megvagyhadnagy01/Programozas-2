2b1f254927155088416a78b56c5e0a51497818b9
megvagyhadnagy01 <38715272+megvagyhadnagy01@users.noreply.github.com>
2018-10-03 19:00:46 +0200

    Add files via upload

---
+++ gagyipr.java
@@ -0,0 +1,22 @@
+
+public class gagyipr {
+
+	
+
+		public static void main (String[]args)
+		{
+
+		Integer x = -128;
+		Integer t = -128;
+
+		System.out.println (x);
+		System.out.println (t);
+
+		while (x <= t && x >= t && t != x);
+
+		}
+
+	}
+
+
+
