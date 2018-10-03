c4d69ae233915d4d47aed220c4d719741c84f9a2
megvagyhadnagy01 <38715272+megvagyhadnagy01@users.noreply.github.com>
2018-10-03 19:04:52 +0200

    Add files via upload

---
+++ GagyiVegtelen.java
@@ -0,0 +1,17 @@
+public class GagyiVegtelen
+{
+
+	public static void main (String[]args)
+	{
+
+	Integer x = -129;
+	Integer t = -129;
+
+	System.out.println (x);
+	System.out.println (t);
+
+	while (x <= t && x >= t && t != x);
+
+	}
+
+}
